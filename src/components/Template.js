f70a9e0199370ad9e1d4755265bb0f75637477d2
yusunjo <tnswh2023@gmail.com>
2020-12-19 11:20:47 +0900

    fixed

--- src/components/Template.js
+++ src/components/Template.js
@@ -4,7 +4,6 @@ const Template= ({children, todoLength}) => {
     return (
         <div className="Template">
             <div className="title">오늘의 할 일({todoLength})</div>
-            <div>할일이 여기에 들어갑니다. </div>
     <div>{children}</div>
         </div>
     )
